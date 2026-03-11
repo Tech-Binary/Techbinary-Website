2e69df4105be605c4d7090c7732f995e422d8e3a
Sanjana <ghelotiasanjana2003@gmail.com>
2026-03-11 16:33:25 +0530

    changes

--- src/Pages/Sop.jsx
+++ src/Pages/Sop.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
-
 const Sop = () => {
-  useEffect(() => {
-    window.location.href = "/sop.pdf";
-  }, []);
-
-  return null;
+  return (
+    <iframe
+      src="/sop.pdf"
+      width="100%"
+      height="1000px"
+      title="SOP Document"
+    />
+  );
 };
 
 export default Sop;
